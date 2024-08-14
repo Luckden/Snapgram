c9d1266af1fc4134c777572634508ccaf6d1cc8b
Luckden <cheahjityung@gmail.com>
2024-08-14 14:53:57 +0800

    Create Post Functionality Added

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
-import { ID, Query } from "appwrite";
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, databases } from "./config";
+import { ID, ImageGravity, Query } from "appwrite";
+import { INewPost, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, databases, storage } from "./config";
 
 export async function createUserAccount(user: INewUser){
     try{
@@ -89,4 +89,98 @@ export async function signOutAccount(){
         console.log(err);
     }
     
+}
+
+export async function createPost(post: INewPost) {
+    try {
+      // Upload file to appwrite storage
+      const uploadedFile = await uploadFile(post.file[0]);
+  
+      if (!uploadedFile) throw Error;
+  
+      // Get file url
+      const fileUrl = await getFilePreview(uploadedFile.$id);
+      if (!fileUrl) {
+        await deleteFile(uploadedFile.$id);
+        throw Error;
+      }
+  
+      // Convert tags into array
+      const tags = post.tags?.replace(/ /g, "").split(",") || [];
+  
+      // Create post
+      const newPost = await databases.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        ID.unique(),
+        {
+          creator: post.userId,
+          caption: post.caption,
+          ImageURL: fileUrl,
+          imageId: uploadedFile.$id,
+          location: post.location,
+          tags: tags,
+        }
+      );
+  
+      if (!newPost) {
+        await deleteFile(uploadedFile.$id);
+        throw Error;
+      }
+  
+      return newPost;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    );
+
+    return uploadedFile;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getFilePreview(fileId: string){
+    try{
+        const fileUrl = storage.getFilePreview(
+            appwriteConfig.storageId,
+            fileId,
+            2000,
+            2000,
+            ImageGravity.Top,
+            100
+        )
+        return fileUrl;
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+async function deleteFile(fileId: string) {
+    try{
+        await storage.deleteFile(appwriteConfig.storageId, fileId);
+        return {status: 'ok'}
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+export async function getRecentPosts(){
+    const posts = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.postCollectionId,
+        [Query.orderDesc('$createdAt'), Query.limit(20)]
+    )
+    if (!posts) throw Error;
+    return posts;
 }
--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -4,7 +4,7 @@ export const appwriteConfig = {
     url: import.meta.env.VITE_APPWRITE_URL,
     projectId: import.meta.env.VITE_APPWRTITE_API_KEY,
     databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
-    storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+    storageId: "6673f0970025993f251d",
     userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
     postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
     savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
